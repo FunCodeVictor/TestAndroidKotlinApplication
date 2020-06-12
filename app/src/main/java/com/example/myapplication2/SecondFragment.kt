1bd1366fbe2b348c44ca4552ed051e5038a8b02d
Victor Ryding <victorfryding@gmail.com>
2020-06-12 18:16:49 +0200

    Everything

---
+++ app/src/main/java/com/example/myapplication2/SecondFragment.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+}
