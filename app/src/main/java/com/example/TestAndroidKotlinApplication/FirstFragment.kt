bea78cab701d5d94ac544567cb53e09299bd66ff
Victor Ryding <victorfryding@gmail.com>
2020-06-12 18:28:28 +0200

    TestAndroidKotlinApplication name

---
+++ app/src/main/java/com/example/TestAndroidKotlinApplication/FirstFragment.kt
@@ -0,0 +1,10 @@
+package com.example.TestAndroidKotlinApplication
+
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+}
---
+++ app/src/main/java/com/example/TestAndroidKotlinApplication/SecondFragment.kt
@@ -0,0 +1,10 @@
+package com.example.TestAndroidKotlinApplication
+
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+}
