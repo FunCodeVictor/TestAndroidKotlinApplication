3a0b3400f5f6c5f31b4072934e4b34d7efa35bf8
Victor Ryding <victorfryding@gmail.com>
2020-10-31 19:33:54 +0100

    TestAndroidKotlinApplication name

--- app/src/main/java/com/example/TestAndroidKotlinApplication/MainActivity.kt
+++ app/src/main/java/com/example/TestAndroidKotlinApplication/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.TestAndroidKotlinApplication
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -49,5 +54,23 @@ class MainActivity : AppCompatActivity() {
             //tes
         }
     }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channelTest)
+            val descriptionText = getString(R.string.channelDesc)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("88", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 
 }
